aca12bc40bf2a3cb9d2540f3e050d4bd3fac1010
Kevin S. Miller <kmille39@its.jnj.com>
2026-02-01 10:18:41 -0500

    Adjusted Ports for for work/home PC

--- src/RpaAlm.Shared/RpaAlm.Shared.Models/Responses/ApiResponse.cs
+++ src/RpaAlm.Shared/RpaAlm.Shared.Models/Responses/ApiResponse.cs
@@ -3,7 +3,10 @@ namespace RpaAlm.Shared.Models.Responses;
 public class ApiResponse<T>
 {
     public bool Success { get; set; }
+
     public T? Data { get; set; }
+
     public string? Message { get; set; }
+
     public List<string>? Errors { get; set; }
 }
